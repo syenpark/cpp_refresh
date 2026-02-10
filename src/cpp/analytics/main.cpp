eaeaaba500e2b6f3b931336c49493cfdf46380fd
Syen Park <syen.park@connect.ust.hk>
2026-02-10 17:26:23 +0800

    feat(cpp): connect zmq

--- src/cpp/analytics/main.cpp
+++ src/cpp/analytics/main.cpp
@@ -2,15 +2,36 @@
 #include <string>
 #include <utility>
 
+#include <zmq.hpp>
+
 #include "common/config.h"
 
 int main(int argc, char **argv) {
+  // ---------- config ----------
   std::string config_path = "config.toml";
 
   if (argc > 1) {
     config_path = argv[1];
   }
   Config cfg = load_config(config_path);
+
+  // ---------- zmq init ----------
+  zmq::context_t ctx{1};
+
+  zmq::socket_t socket(ctx, zmq::socket_type::sub);
+
+  socket.set(zmq::sockopt::rcvhwm, cfg.zmq.rcvhwm);
+  socket.set(zmq::sockopt::subscribe, cfg.zmq.subscribe);
+
+  socket.connect(cfg.zmq.endpoint);
+
+  std::cout << "Connected to " << cfg.zmq.endpoint << "\n";
+
+  // ---------- recv test ----------
+  zmq::message_t msg;
+  socket.recv(msg, zmq::recv_flags::none);
+
+  std::cout << "Received message size=" << msg.size() << "\n";
 
   std::cout << "[config]\n";
   std::cout << "  max_sources: " << cfg.analytics.max_sources << "\n";
